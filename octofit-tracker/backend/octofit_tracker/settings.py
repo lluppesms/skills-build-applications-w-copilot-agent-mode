c8618e9551f64d16f02a92a68931a933e8bebcf0
Lyle (Microsoft) Luppes <110034725+lluppesms@users.noreply.github.com>
2025-04-08 21:11:53 +0000

    Fourth step

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!+60$$gi$x==w(9g@#c!zdp!tn61=20l7c^l7r)s&bwb&#(=^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "special-goggles-5jx6995979qh467v-8000.app.github.dev"]
 
 
 # Application definition - what's missing?
